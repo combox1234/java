a7d72a9149fcf9a3ff8ba5ae47b403ade5df8856
parth patil <146315334+combox1234@users.noreply.github.com>
2025-06-15 20:03:04 +0530

    asmd.java
    contains all the methods for calculator

---
+++ calculator/methods/asmd.java
@@ -0,0 +1,22 @@
+package methods;
+
+public class asmd {
+    private double a,b;
+    public asmd(double x,double y){
+        this.a = x;
+        this.b = y;
+    }
+    public double add(){
+        return a+b;
+    }
+    public double sub(){
+        return a-b;
+    }
+    public double mul(){
+        return a*b;
+    }
+    public double div(){
+        return a/b;
+    }
+
+}
